3ff8eb6ba1e62e17a2326ff9e39d330016300c86
PaulosNed <93246229+PaulosNed@users.noreply.github.com>
2022-01-26 22:15:16 +0300

    Delete AdminRepository.java

--- event_notifier/src/main/java/com/group/event_notifier/security/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.group.event_notifier.security;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AdminRepository extends CrudRepository<Admin, Long> {
-    public Admin findByUsername(String username);
-}
